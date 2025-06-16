4cea1524f5ca1f66262d6c3b468a238dc8ad2562
gipicolott <picolottart@gmail.com>
2025-06-16 16:50:27 -0300

    Terminei a pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,38 @@ menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener("click", () => {
-    document.body.classList.remove("show-mobile-menu");
-});
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 
+
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+  // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+    },
+
+  // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+  // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1 
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
